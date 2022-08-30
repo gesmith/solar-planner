5690baba00a97131fa5e0261cb7ae913a1829483
Garry Smith <ronintek0@gmail.com>
2022-08-30 17:00:00 -0400

    add mapbox-gl-draw and enable polygon controls

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,7 +1,11 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './Map.css';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
- 
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NjcmlwdCIsImEiOiJjbDdnN2Z0cGUwMXNmM25vZnJnbHBmYXY1In0.webVjZGyliDaSaf7ZEAI9Q';
 
 export default function Map() {
@@ -11,6 +15,19 @@ export default function Map() {
     const [lat, setLat] = useState(42.35);
     const [zoom, setZoom] = useState(9);
 
+
+    const draw = new MapboxDraw({
+        displayControlsDefault: false,
+        // Select which mapbox-gl-draw control buttons to add to the map.
+        controls: {
+            polygon: true,
+            trash: true
+        },
+        // Set mapbox-gl-draw to draw by default.
+        // The user does not have to click the polygon control button first.
+        // defaultMode: 'draw_polygon'
+    });
+    
     useEffect(() => {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
@@ -19,6 +36,15 @@ export default function Map() {
             center: [lng, lat],
             zoom: zoom
         });
+        // Add the control to the map.
+        map.current.addControl(
+            new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl
+            })
+        );
+        
+        map.current.addControl(draw, 'top-left');
     });
 
     useEffect(() => {
