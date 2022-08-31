b9f0952c84615bc43aabb6c2a2c2987ade77b923
Garry Smith <ronintek0@gmail.com>
2022-08-31 13:12:51 -0400

    update eslint rules

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  }
-}
---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+export default {
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-debugger': 'off'
+  }
+};
