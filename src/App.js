c5c82df57f4dc8ffc843a119174119b215157736
Garry Smith <ronintek0@gmail.com>
2022-08-31 08:26:20 -0400

    change layout to a two column layout with map and sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import './App.css';
-import Map from './Map'
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Map from './Map';
+import Sidebar from './Sidebar';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Map />
-    </div>
+    <Grid container component="main" sx={{ height: '100vh' }}>
+      <CssBaseline />
+      <MuiAppBar position="absolute">
+        <Toolbar>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Solar Planner
+          </Typography>
+        </Toolbar>
+      </MuiAppBar>
+      <Grid
+        sx={{
+          flexGrow: 1,
+          height: '100vh',
+          overflow: 'auto'
+        }}
+      >
+        <Toolbar />
+        <Container>
+          <Map />
+        </Container>
+      </Grid>
+      <Sidebar />
+    </Grid>
   );
 }
-
-export default App;
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,64 +1,57 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './Map.css';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NjcmlwdCIsImEiOiJjbDdnN2Z0cGUwMXNmM25vZnJnbHBmYXY1In0.webVjZGyliDaSaf7ZEAI9Q';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZ3NjcmlwdCIsImEiOiJjbDdnN2Z0cGUwMXNmM25vZnJnbHBmYXY1In0.webVjZGyliDaSaf7ZEAI9Q';
 
 export default function Map() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
+  const draw = new MapboxDraw({
+    displayControlsDefault: false,
+    // Select which mapbox-gl-draw control buttons to add to the map.
+    controls: {
+      polygon: true,
+      trash: true
+    }
+  });
 
-    const draw = new MapboxDraw({
-        displayControlsDefault: false,
-        // Select which mapbox-gl-draw control buttons to add to the map.
-        controls: {
-            polygon: true,
-            trash: true
-        },
-        // Set mapbox-gl-draw to draw by default.
-        // The user does not have to click the polygon control button first.
-        // defaultMode: 'draw_polygon'
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
     });
-    
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-        });
-        // Add the control to the map.
-        map.current.addControl(
-            new MapboxGeocoder({
-                accessToken: mapboxgl.accessToken,
-                mapboxgl: mapboxgl
-            })
-        );
-        
-        map.current.addControl(draw, 'top-left');
-    });
-
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-    });
-
-    return (
-        <div>
-            <div ref={mapContainer} className="map-container" />
-        </div>
+    // Add the control to the map.
+    map.current.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+      })
     );
+
+    map.current.addControl(draw, 'top-left');
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return <div ref={mapContainer} className="map-container" />;
 }
